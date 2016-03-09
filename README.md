ee167b2d89e1f8ee76baf02728eba4a7229117a9
Gidon Ernst <ernst@informatik.uni-augsburg.de>
2016-03-09 09:34:42 +0100

    initial commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+TST Library
+===========
+
+Minimal Unit Testing Library.
+
+Author: Gidon Ernst <gidonernst@gmail.com>
+
+Feedback is welcome! I'd appreciate to hear whether anyone found this library useful.
+
+Motivation & Overview
+---------------------
+
+Inspired by [minunit](#).
+[ScalaTest](http://scalatest.org) is too large for my taste.
+
+The library consists of a single trait, `Test`, that is inherited by test suites.
+You can then write
+
+    test("simple examples) {
+        x expect 2
+        x inspect { _ % 2 == 0 }
+        x throws[IOException]
+    }
+
+Quick Reference
+
+- `x expect y` compares an actual value `x` to an expected one `y` and requires
+  these to be equal
+- `x inspect p` requires that the result of `x` satisfies predicate `p`
+- `x throws[E]` requires that evaluating `x` throws an exception of the
+  indicated type `E` (or one of its subtypes)
+- `abort(msg)` aborts a running test with a given message
+- Feel free to add your own checkers, throw a `Bug(msg)` exception on failure
+
+You will get a summary for each test set up in this way,
+indicating the first failed assertion resp. success of the entire test
+plus some timing information.
+Note that `expect` and `inspect` fail when `x` throws an exception.
---
+++ src/tst/Test.scala
@@ -0,0 +1,67 @@
+// TST Unit Test Library
+// (c) 2015 Gidon Ernst <gidonernst@gmail.com>
+// This code is licensed under MIT license (see LICENSE for details)
+
+package tst
+
+import scala.reflect.ClassTag
+
+object Test {
+  def time[A](f: => A): (A, Long) = {
+    val start = System.currentTimeMillis
+    val r = f
+    val end = System.currentTimeMillis
+    (r, end - start)
+  }
+
+  case class Bug(override val toString: String) extends Exception
+  case class Abort(override val toString: String) extends Exception
+}
+
+trait Test {
+  import Test._
+
+  implicit class Assertions[A](a: => A) {
+    def expect[B <: A](b: B) {
+      if (a != b)
+        throw Bug("actual: " + a + ", expected: " + b)
+    }
+
+    def inspect(p: Any => Boolean) {
+      try {
+        if (!p(a))
+          throw Bug("actual: " + a + " is unexpected (malformed)")
+      } catch {
+        case _: MatchError =>
+          throw Bug("actual: " + a + " is unexpected (no match)")
+      }
+    }
+
+    def throws[E <: Throwable](implicit ev: ClassTag[E]) {
+      val r = try {
+        a
+      } catch {
+        case _: E => return
+        case r: Throwable => r
+      }
+      throw Bug("actual: " + r + ", expected: " + ev)
+    }
+  }
+
+  def abort(reason: String) = throw Abort(reason)
+
+  def test(name: String)(f: => Any) = {
+    try {
+      val (_, t) = time(f)
+      System.out.println("'" + name + "' passed in " + t + "ms")
+    } catch {
+      case Bug(message) =>
+        System.err.println("'" + name + "' failed: " + message)
+      case Abort(reason) =>
+        System.err.println("'" + name + "' was aborted: " + reason)
+      case e: Throwable =>
+        System.err.println("'" + name + "' failed: " + e + " (" + e.getClass.getSimpleName + ")")
+      // throw e
+    }
+  }
+}
